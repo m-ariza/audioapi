22ca0dff33f67531c48b4776c21df7ba615da471
m-ariza <m.ariza214@gmail.com>
2018-02-26 19:39:43 -0500

    Add files via upload

--- js/waveform.js
+++ js/waveform.js
@@ -1,60 +1,7 @@
-function waveform(fValue, color) {
-    console.log('wafevorm');
-
-    var audioContext = new(window.AudioContext || window.webkitAudioContext);
-    var song = document.getElementById('song');
-    var scopeCanvas = document.getElementById('oscilloscope');
-    var scopeContext = scopeCanvas.getContext('2d');
-    var analyser = audioContext.createAnalyser();
-    var songSource = audioContext.createMediaElementSource(song);
-    var waveform = new Float32Array(analyser.frequencyBinCount);
-
-    var filter = audioContext.createBiquadFilter();
-
-    songSource.connect(filter);
-    filter.connect(analyser);
-    filter.frequency.value = fValue;
-    scopeCanvas.height = 2800;
-    scopeCanvas.width = waveform.length;
-    var masterGain = audioContext.createGain();
-    masterGain.connect(audioContext.destination);
-    song.crossOrigin = 'anonymous';
-    songSource.connect(masterGain);
-
-
-    analyser.getFloatTimeDomainData(waveform);
-    (function updateWaveform() {
-        requestAnimationFrame(updateWaveform);
-        analyser.getFloatTimeDomainData(waveform);
-
-    })();
-    (function drawOscilloscope() {
-        requestAnimationFrame(drawOscilloscope);
-        scopeContext.clearRect(0, 0, scopeCanvas.width, scopeCanvas.height);
-        scopeContext.beginPath();
-
-
-        for (var i = 0; i < waveform.length; i++) {
-            const x = i;
-            const y = (0.5 + waveform[i]) * scopeCanvas.height;
-            if (i == 0) {
-                scopeContext.moveTo(x, y);
-            } else {
-                scopeContext.lineTo(x, y);
-            }
-        }
-        scopeContext.lineWidth = 3;
-        scopeContext.strokeStyle = color;
-        scopeContext.stroke();
-    })();
-
-
-}
-
 function waveform2() {
     console.log('wafevorm2');
 
-    var audioContext = new(window.AudioContext || window.webkitAudioContext);
+    var audioContext = new window.AudioContext() || window.webkitAudioContext();
     var song = document.getElementById('song');
     var songSource = audioContext.createMediaElementSource(song);
 
@@ -65,20 +12,24 @@ function waveform2() {
     var analyser2 = audioContext.createAnalyser();
     var waveform = new Float32Array(analyser.frequencyBinCount);
     var waveform2 = new Float32Array(analyser2.frequencyBinCount);
-
+    var gainNode = audioContext.createGain();
+    analyser2.fftSize = 256;
+    analyser.fftSize = 512;
     var filter = audioContext.createBiquadFilter();
     var filter2 = audioContext.createBiquadFilter();
-
-    songSource.connect(splitter);
+    filter.Q.value = 0;
+    filter2.Q.value = 0;
+    songSource.connect(gainNode);
+    gainNode.gain.setValueAtTime(0.4, audioContext.currentTime);
+    gainNode.connect(splitter);
     splitter.connect(filter, 0);
     splitter.connect(filter2, 1, 0);
     filter.connect(analyser, 0);
     filter2.connect(analyser2, 0, 0);
 
-    filter.frequency.value = 50;
-    filter2.frequency.value = 1600;
-
-    scopeCanvas.height = 2500;
+    filter.frequency.value = 100;
+    filter2.frequency.value = 2500;
+    scopeCanvas.height = 2000;
     scopeCanvas.width = waveform.length;
     var masterGain = audioContext.createGain();
     songSource.connect(masterGain);
@@ -98,41 +49,52 @@ function waveform2() {
         scopeContext.clearRect(0, 0, scopeCanvas.width, scopeCanvas.height);
         scopeContext.beginPath();
         for (var i = 0; i < waveform.length; i++) {
-            const x = i;
-            const y = (0.5 + waveform[i]) * scopeCanvas.height;
+            const x = i * 1;
+            const y = (0.5 + waveform[i / 2]) * (scopeCanvas.height);
             if (i == 0) {
                 scopeContext.moveTo(x, y);
             } else {
                 scopeContext.lineTo(x, y);
             }
         }
-        scopeContext.lineWidth = 25;
-        scopeContext.strokeStyle = 'rgba(255, 255, 255, .30)';
+        scopeContext.lineWidth = 40;
+        scopeContext.strokeStyle = 'rgba(255,255, 255, 1)';
         scopeContext.stroke();
+        scopeContext.shadowOffsetX = 30;
+        scopeContext.shadowOffsetY = 20;
+        scopeContext.shadowBlur = 1;
+        scopeContext.shadowColor = 'rgba(5, 5, 5, .8 )';
+        scopeContext.lineJoin = 'round';
+
     })();
+    console.log(scopeCanvas.height);
+
     (function drawOscilloscope2() {
+
         requestAnimationFrame(drawOscilloscope2);
         scopeContext.beginPath();
         for (var i = 0; i < waveform2.length; i++) {
-            const x = i * 10;
-            const y = (0.5 + waveform2[i]) * (scopeCanvas.height);
+            const x = i;
+            const y = (0.5 + waveform2[i / 10]) * (scopeCanvas.height);
             if (i == 0) {
                 scopeContext.moveTo(x, y);
             } else {
                 scopeContext.lineTo(x, y);
             }
         }
-        scopeContext.lineWidth = 20;
-        scopeContext.strokeStyle = 'rgba(200, 50, 100, 1';
-        scopeContext.textAlign = 'center';
+        scopeContext.lineWidth = 30;
+        scopeContext.strokeStyle = 'rgba(150, 5, 40, 1';
+
         scopeContext.stroke();
-        scopeContext.shadowOffsetX = 5;
-        scopeContext.shadowOffsetY = 10;
-        scopeContext.shadowBlur = 2;
-        scopeContext.shadowColor = 'rgba(0, 0, 0, 1)';
+        scopeContext.shadowOffsetX = 0;
+        scopeContext.shadowOffsetY = 0;
+        scopeContext.shadowBlur = 1;
+        scopeContext.shadowColor = 'rgba(55, 0,0, 0)';
+        scopeContext.lineJoin = 'round';
+
+
     })();
 
 
 }
-//waveform(50, '#2255dd');
 waveform2();
