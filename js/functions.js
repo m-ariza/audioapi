89d5921d99f5b00dbac6f9641d99204cbc33119e
m-ariza <m.ariza214@gmail.com>
2018-03-02 16:52:44 -0500

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,100 +1,109 @@
-var $play = $('#pButton');
-var $stop = $('#stop');
-var song = document.getElementById('song');
-var $track = $('#playlist').find('li');
-var $playlistBtn = $('#playlist-btn');
-var $playhead = $('#playhead');
-song.controls = false; //dont show controls
-function playAudio() { /*function definition for pause/play button*/
+(function strict() {
+    "use strict";
 
-    if (song.paused) {
-        song.play();
-        $('#pButton').html('pause');
-    } else {
-        song.pause();
-        $('#pButton').html('play_arrow');
+    var $play = $('#pButton');
+    var $stop = $('#stop');
+    var $timeline = $('#timeline');
+    var song = document.getElementById('song');
+    var $track = $('#playlist').find('li');
+    var $playlistBtn = $('#playlist-btn');
+    var $playhead = $('#playhead');
+    song.controls = false; //dont show controls
+    function playAudio() { /*function definition for pause/play button*/
+
+        if (song.paused) {
+            song.play();
+            $('#pButton').html('pause');
+        } else {
+            song.pause();
+            $('#pButton').html('play_arrow');
+        }
     }
-}
 
 
-song.onended = function() { /*when song ends change STOP button to PLAY*/
-    $('#pButton').html('play_arrow');
-};
-
-
-
-$track.click(function() {
-    $(song).find('source').attr('src', $(this).attr('data-src'));
-    /*set the song's src attr
-    to the clicked track element's
-    custom data-src attr*/
-    song.load(); //load clicked track
-    playAudio(); //play song
-    $track.each(function() { //hide playlist
-        $track.slideUp();
-    });
-});
-$playlistBtn.click(function() {
-    $track.slideToggle();
-});
-
-
-
-
-$play.click(function() {
-    playAudio(); //play loaded audio
-});
-$stop.click(function() {
-    if (song.play) {
-        song.pause();
-        song.load();
+    song.onended = function() { /*when song ends change STOP button to PLAY*/
         $('#pButton').html('play_arrow');
-    }
-});
+    };
 
 
 
-
-/*function for timeline slider to move on timeupdate event listener*/
-function timeUpdate() {
-    var $playPercent = (song.currentTime / song.duration) * 100;
-
-    $('#playhead').css({
-        "margin-left": $playPercent + "%"
+    $track.click(function() {
+        $(song).find('source').attr('src', $(this).attr('data-src'));
+        /*set the song's src attr
+        to the clicked track element's
+        custom data-src attr*/
+        song.load(); //load clicked track
+        playAudio(); //play song
+        $track.each(function() { //hide playlist
+            $track.slideUp();
+        });
     });
-}
-song.addEventListener('timeupdate', timeUpdate);
+    $playlistBtn.click(function() {
+        $track.slideToggle();
+    });
 
-function clickPercent(e) { //gets decimal value of position clicked
-    return e.clientX / $(this).width()
-}
 
-function movePlayhead(e) {
-    $playhead.mousedown(function() {
-        song.removeEventListener('timeupdate', timeUpdate);
 
-        $(window).mousemove(function(e) {
-            var $playPercent = (e.clientX / $timeline.width() * 100);
 
-            $('#playhead').css({
-                "margin-left": $playPercent + "%"
+    $play.click(function() {
+        playAudio(); //play loaded audio
+    });
+    $stop.click(function() {
+        if (song.play) {
+            song.pause();
+            song.load();
+            $('#pButton').html('play_arrow');
+        }
+    });
+
+
+
+
+    /*function for timeline slider to move on timeupdate event listener*/
+    function timeUpdate() {
+        var $playPercent = (song.currentTime / song.duration) * 100;
+
+        $('#playhead').css({
+            "margin-left": $playPercent + "%"
+        });
+    }
+    song.addEventListener('timeupdate', timeUpdate);
+
+    function clickPercent(e) { //gets decimal value of position clicked
+        return e.clientX / $timeline.width();
+    }
+
+    function movePlayhead(e) {
+        $playhead.mousedown(function() {
+            song.removeEventListener('timeupdate', timeUpdate);
+
+            $(window).mousemove(function(e) {
+                var $playPercent = (e.clientX / $timeline.width() * 100);
+
+                $('#playhead').css({
+                    "margin-left": $playPercent + "%"
+                });
+
             });
 
         });
+        $(window).mouseup(function() {
+            $(window).off('mousemove');
 
-    });
-    $(window).mouseup(function() {
-        "use strict";
-        song.addEventListener('timeupdate', timeUpdate);
-        $(window).off('mousemove');
-        console.log('mouseup');
+        });
+        $timeline.mouseup(function() {
+
+            song.addEventListener('timeupdate', timeUpdate);
+            $(window).off('mousemove');
+            console.log('mouseup');
+            song.currentTime = (song.duration * clickPercent(e));
+
+        });
+    }
+
+    $timeline.click(function(e) {
         song.currentTime = (song.duration * clickPercent(e));
-
-    })
-}
-var $timeline = $('#timeline');
-
-$timeline.click(function(e) {
-    song.currentTime = (song.duration * clickPercent(e));
-    movePlayhead(e);
-});
+        movePlayhead(e);
+        console.log('tl clicked');
+    });
+}());
--- js/waveform.js
+++ js/waveform.js
@@ -13,22 +13,22 @@ function waveform2() {
     var waveform = new Float32Array(analyser.frequencyBinCount);
     var waveform2 = new Float32Array(analyser2.frequencyBinCount);
     var gainNode = audioContext.createGain();
-    analyser2.fftSize = 256;
-    analyser.fftSize = 512;
+    analyser2.fftSize = 256; //vocals
+    analyser.fftSize = 1024;
     var filter = audioContext.createBiquadFilter();
     var filter2 = audioContext.createBiquadFilter();
-    filter.Q.value = 0;
-    filter2.Q.value = 0;
+    filter.Q.value = 10;
+    filter2.Q.value = 10;
     songSource.connect(gainNode);
-    gainNode.gain.setValueAtTime(0.4, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
     gainNode.connect(splitter);
     splitter.connect(filter, 0);
     splitter.connect(filter2, 1, 0);
     filter.connect(analyser, 0);
     filter2.connect(analyser2, 0, 0);
 
-    filter.frequency.value = 100;
-    filter2.frequency.value = 2500;
+    filter.frequency.value = 80;
+    filter2.frequency.value = 2000;
     scopeCanvas.height = 2000;
     scopeCanvas.width = waveform.length;
     var masterGain = audioContext.createGain();
@@ -50,32 +50,32 @@ function waveform2() {
         scopeContext.beginPath();
         for (var i = 0; i < waveform.length; i++) {
             const x = i * 1;
-            const y = (0.5 + waveform[i / 2]) * (scopeCanvas.height);
+            const y = (0.5 + waveform[i / 1]) * (scopeCanvas.height);
             if (i == 0) {
                 scopeContext.moveTo(x, y);
             } else {
                 scopeContext.lineTo(x, y);
             }
         }
-        scopeContext.lineWidth = 40;
+        scopeContext.lineWidth = 80;
         scopeContext.strokeStyle = 'rgba(255,255, 255, 1)';
         scopeContext.stroke();
         scopeContext.shadowOffsetX = 30;
         scopeContext.shadowOffsetY = 20;
         scopeContext.shadowBlur = 1;
-        scopeContext.shadowColor = 'rgba(5, 5, 5, .8 )';
+        scopeContext.shadowColor = 'rgba(5, 5, 5, 1 )';
         scopeContext.lineJoin = 'round';
 
     })();
     console.log(scopeCanvas.height);
 
     (function drawOscilloscope2() {
-
+        //red waveform for vocals
         requestAnimationFrame(drawOscilloscope2);
         scopeContext.beginPath();
         for (var i = 0; i < waveform2.length; i++) {
             const x = i;
-            const y = (0.5 + waveform2[i / 10]) * (scopeCanvas.height);
+            const y = (0.5 + waveform2[i / 4]) * (scopeCanvas.height);
             if (i == 0) {
                 scopeContext.moveTo(x, y);
             } else {
@@ -83,7 +83,7 @@ function waveform2() {
             }
         }
         scopeContext.lineWidth = 30;
-        scopeContext.strokeStyle = 'rgba(150, 5, 40, 1';
+        scopeContext.strokeStyle = 'rgba(150, 5, 40, .7';
 
         scopeContext.stroke();
         scopeContext.shadowOffsetX = 0;
